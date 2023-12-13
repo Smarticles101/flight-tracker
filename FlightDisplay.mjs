85634811e78e5ce44d54bcef60bf441edb799581
Logan Stucki <loganstucki@gmail.com>
2023-12-13 14:15:06 -0600

    display flight info

--- FlightDisplay.mjs
+++ FlightDisplay.mjs
@@ -1,8 +1,20 @@
 import { getFlightSchedule } from "./api";
 
 const flightDisplayHTML = `
-<div>
 <input placeholder="flight number"> <button id="input_submit">Submit</button>
+<div id="flight-info-display">
+<div id="departing">
+<p class="airport"></p>
+<p class="terminal"></p>
+<p class="gate"></p>
+<p class="time"></p>
+</div>
+<div id="arriving">
+<p class="airport"></p>
+<p class="terminal"></p>
+<p class="gate"></p>
+<p class="time"></p>
+</div>
 </div>`;
 
 export default class FlightDisplay {
@@ -18,10 +30,24 @@ export default class FlightDisplay {
       .addEventListener("click", this.submit.bind(this));
   }
 
+  updateDisplay(data) {
+    this.container.querySelector("#departing .airport").innerHTML = `Departure Airport ${data.dep_iata}`;
+    this.container.querySelector("#departing .terminal").innerHTML = `Departure Terminal ${data.dep_terminal}`;
+    this.container.querySelector("#departing .gate").innerHTML = `Departure Gate ${data.dep_gate}`;
+    this.container.querySelector("#departing .time").innerHTML = `Departure Time ${data.dep_time}`;
+
+    this.container.querySelector("#arriving .airport").innerHTML = `Arrival Airport ${data.arr_iata}`;
+    this.container.querySelector("#arriving .terminal").innerHTML = `Arrival Terminal ${data.arr_terminal}`;
+    this.container.querySelector("#arriving .gate").innerHTML = `Arrival Gate ${data.arr_gate}`;
+    this.container.querySelector("#arriving .time").innerHTML = `Arrival Time ${data.arr_time}`;
+
+    console.log(data);
+  }
+
   submit() {
     let flight_code = this.container.querySelector("input").value;
     this.container.querySelector("input").value = "";
 
-    getFlightSchedule(flight_code).then(r => console.log(r));
+    getFlightSchedule(flight_code).then(this.updateDisplay.bind(this));
   }
 }
