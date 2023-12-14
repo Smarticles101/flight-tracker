b3cf92fe52d6edc902c9010da9c00805084d0d5e
Logan Stucki <loganstucki@gmail.com>
2023-12-13 18:39:32 -0600

    add simple loading animation

--- FlightDisplay.mjs
+++ FlightDisplay.mjs
@@ -2,6 +2,13 @@ import { FLIGHT_INFO_HTML, getFlightSchedule, storeFlight, updateDisplay } from 
 
 const flightDisplayHTML = `
 <input placeholder="flight number"> <button id="input_submit">Submit</button>
+<div class="loading-text"> <!--Loading-text-->
+    <p>LOADING
+      <span class="dot-one"> .</span>
+      <span class="dot-two"> .</span>
+      <span class="dot-three"> .</span>
+    </p>
+  </div> <!--/Loading-text-->
 ${FLIGHT_INFO_HTML}
 </div>`;
 
@@ -21,9 +28,11 @@ export default class FlightDisplay {
   submit() {
     let flight_code = this.container.querySelector("input").value;
     this.container.querySelector("input").value = "";
+    this.container.querySelector(".loading-text").style.display = "block";
 
     getFlightSchedule(flight_code).then(data => {
       // TODO: if no flight, data is undef. handle error
+      this.container.querySelector(".loading-text").style.display = "none";
       updateDisplay(this.container, data)
       storeFlight(data)
     });
